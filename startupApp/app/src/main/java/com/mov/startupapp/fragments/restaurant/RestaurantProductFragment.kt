dc6b7098b736660975eb261f597321b0d9d3268f
dmmoraless <100449711+dmmoraless@users.noreply.github.com>
2022-03-09 19:25:50 -0500

    Creando el formulario para crear el producto

--- startupApp/app/src/main/java/com/mov/startupapp/fragments/restaurant/RestaurantProductFragment.kt
+++ startupApp/app/src/main/java/com/mov/startupapp/fragments/restaurant/RestaurantProductFragment.kt
@@ -1,61 +1,100 @@
 package com.mov.startupapp.fragments.restaurant
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.github.dhaval2404.imagepicker.ImagePicker
 import com.mov.startupapp.R
+import java.io.File
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RestaurantProductFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RestaurantProductFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var myView: View? = null
+    var editTextName: EditText? = null
+    var editTextDescription: EditText? = null
+    var editTextPrice: EditText? = null
+    var imageViewProduct1: ImageView? = null
+    var imageViewProduct2: ImageView? = null
+    var imageViewProduct3: ImageView? = null
+    var buttonCreate: Button? = null
+
+    var imageFile1: File? = null
+    var imageFile2: File? = null
+    var imageFile3: File? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_product, container, false)
+
+        myView = inflater.inflate(R.layout.fragment_restaurant_product, container, false)
+
+        editTextName = myView?.findViewById(R.id.edittext_name)
+        editTextDescription = myView?.findViewById(R.id.edittext_description)
+        editTextPrice = myView?.findViewById(R.id.edittext_price)
+        imageViewProduct1 = myView?.findViewById(R.id.imageview_image1)
+        imageViewProduct2 = myView?.findViewById(R.id.imageview_image2)
+        imageViewProduct3 = myView?.findViewById(R.id.imageview_image3)
+        buttonCreate = myView?.findViewById(R.id.btn_create)
+
+        buttonCreate?.setOnClickListener { createProduct() }
+
+        imageViewProduct1?.setOnClickListener { selectImage(101) }
+        imageViewProduct2?.setOnClickListener { selectImage(102) }
+        imageViewProduct3?.setOnClickListener { selectImage(103) }
+
+        return myView
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RestaurantProductFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RestaurantProductFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun createProduct() {
+        val name = editTextName?.text.toString()
+        val description = editTextDescription?.text.toString()
+        val priceText = editTextPrice?.text.toString()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+
+            val fileUri = data?.data
+
+            if (requestCode == 101) {
+                imageFile1 = File(fileUri?.path) // EL ARCHIVO QUE VAMOS A GUARDAR COMO IMAGEN EN EL SERVIDOR
+                imageViewProduct1?.setImageURI(fileUri)
             }
+            else if (requestCode == 102) {
+                imageFile2 = File(fileUri?.path) // EL ARCHIVO QUE VAMOS A GUARDAR COMO IMAGEN EN EL SERVIDOR
+                imageViewProduct2?.setImageURI(fileUri)
+            }
+
+            else if (requestCode == 103) {
+                imageFile3 = File(fileUri?.path) // EL ARCHIVO QUE VAMOS A GUARDAR COMO IMAGEN EN EL SERVIDOR
+                imageViewProduct3?.setImageURI(fileUri)
+            }
+
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(requireContext(), ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(requireContext(), "Task Cancelled", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun selectImage(requestCode: Int) {
+        ImagePicker.with(this)
+            .crop()
+            .compress(1024)
+            .maxResultSize(1080, 1080)
+            .start(requestCode)
     }
 }
